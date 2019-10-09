a719ef9bfbc446a7f727d070c59be68fbd99caa7
Mei Wong <fongmeiw@gmail.com>
2019-10-09 16:35:22 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+def my_each(collection)
   # code here
+  i = 0
+  while i < collection.size do
+    yield collection[i] if block_given?
+    i += 1
+  end
+  
+  collection
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  p i
 end
