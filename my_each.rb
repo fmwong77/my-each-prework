105fa9bfdb500f0dd48a5a28cc7f207d7fd0c641
Mei Wong <fongmeiw@gmail.com>
2019-10-09 16:34:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,14 @@ def my_each(collection)
   # code here
   i = 0
   while i < collection.size do
-    puts 
+    yield collection[i] if block_given?
     i += 1
   end
+  
+  collection
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
